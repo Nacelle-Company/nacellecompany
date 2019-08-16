e4675fd14cb456f400f47501cc7b8e9579d0ed40
Luke Hartman <lukecarlhartman@gmail.com>
2019-08-16 08:18:38 -0700

    --- Auto-Git Commit ---

--- page-templates/front.php
+++ page-templates/front.php
@@ -8,81 +8,112 @@ get_header();
 
 <?php do_action('Nacelle_before_content'); ?>
 
-<?php
-$image = get_field('gif');
-$seconds = get_field('seconds');
-$secondsHide = $seconds + 1;
-$video_mp4 =  get_field('splash');
-$iframe =  get_field('embed_video');
-$count = 0;
-?>
 
-<?php if ($image) : ?>
-
-<div class="splash">
-	<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-</div>
-
-<?php elseif ($video_mp4) : ?>
-
-<div class="splash fade-in">
-	<?php
-
-		// https://speckyboy.com/html5-video-wordpress-custom-fields/
-		// Get the Video Fields
-
-		// Build the  Shortcode
-		$attr =  array(
-			'mp4'      => $video_mp4,
-			'preload'  => 'auto',
-			'autoplay' => 'on'
-		);
-
-		// Display the Shortcode
-		echo wp_video_shortcode($attr);
-
-
-		?>
-
-	<?php elseif ($iframe) : ?>
-
-	<div class="splash fade-in">
-
-		<div class="embed-container">
-			
-			<?php
-
-			
-				?>
-		</div>
-
-	</div>
-
-	<?php else : ?>
-
-	<?php endif; ?>
-
-	<script>
-		let splash = document.querySelector(".splash");
-		window.addEventListener("load", function() {
-			setTimeout(function() {
-				splash.classList.add("slideLeft");
-
-			}, <?php echo $seconds; ?>000);
-
-		});
-
-		window.addEventListener("load", function() {
-			setTimeout(function() {
-				splash.classList.add("hidden");
-
-			}, <?php echo $secondsHide; ?>000);
-
-		});
-	</script>
-</div>
 <div class="grid-x">
 	<div class="cell">
+
+
+		<?php
+		$image = get_field('gif');
+		$seconds = get_field('seconds');
+		$secondsHide = $seconds + 1;
+		$video_mp4 =  get_field('splash');
+		$iframe =  get_field('embed_video');
+		$count = 0;
+		?>
+
+		<?php if ($image) : ?>
+
+		<div class="splash">
+			<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+		</div>
+
+		<?php elseif ($video_mp4) : ?>
+
+		<div class="splash fade-in">
+			<?php
+
+				// https://speckyboy.com/html5-video-wordpress-custom-fields/
+				// Get the Video Fields
+
+				// Build the  Shortcode
+				$attr =  array(
+					'mp4'      => $video_mp4,
+					'preload'  => 'auto',
+					'autoplay' => 'on'
+				);
+
+				// Display the Shortcode
+				echo wp_video_shortcode($attr);
+
+
+				?>
+		</div>
+		<?php elseif ($iframe) : ?>
+
+		<div class="splash fade-in">
+
+			<div class="embed-container">
+
+				<?php
+
+					// use preg_match to find iframe src
+					preg_match('/src="(.+?)"/', $iframe, $matches);
+					$src = $matches[1];
+
+
+					// add extra params to iframe src
+					$params = array(
+						'controls'    => 0,
+						'hd'        => 1,
+						'autohide'    => 1,
+						'rel' => 0,
+						'modestbranding' => 1,
+						'autoplay' => 1
+
+					);
+
+					$new_src = add_query_arg($params, $src);
+
+					$iframe = str_replace($src, $new_src, $iframe);
+
+
+					// add extra attributes to iframe html
+					$attributes = 'frameborder="0"';
+
+					$iframe = str_replace('></iframe>', ' ' . $attributes . '></iframe>', $iframe);
+
+
+					// echo $iframe
+					echo $iframe;
+
+					?>
+			</div>
+
+		</div>
+
+		<?php else : ?>
+
+		<?php endif; ?>
+
+		<script>
+			let splash = document.querySelector(".splash");
+			window.addEventListener("load", function() {
+				setTimeout(function() {
+					splash.classList.add("slideLeft");
+
+				}, <?php echo $seconds; ?>000);
+
+			});
+
+			window.addEventListener("load", function() {
+				setTimeout(function() {
+					splash.classList.add("hidden");
+
+				}, <?php echo $secondsHide; ?>000);
+
+			});
+		</script>
 
 		<?php while (have_posts('')) : the_post(); ?>
 		<?php get_template_part('template-parts/clean-hero-slider'); ?>
