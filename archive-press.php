fbe28cdbaa260b936ebfcff3501a94fbdad33865
luukee <luke@lukecarlhartman.com>
2021-04-07 16:12:15 -0700

    get_post_meta swap from ACF get_field

--- archive-press.php
+++ archive-press.php
@@ -34,7 +34,8 @@ get_header(); ?>
 				?>
 				<?php while (have_posts()) : the_post();
 
-					$link = get_field('link_to_article');
+					// $link = get_field('link_to_article');
+					$link = get_post_meta(get_the_ID(), 'link_to_article', true);
 					$link_url = $link['url'];
 					$link_title = $link['title'];
 					$link_target = $link['target'] ? $link['target'] : '_self';
@@ -56,44 +57,25 @@ get_header(); ?>
 								<path d="M201.067 0v47.295c72.175 5.884 129.12 66.964 129.12 141.205 0 74.241-56.945 135.32-129.12 141.206V377C299.077 371.024 377 288.769 377 188.5 377 88.232 299.076 5.977 201.067 0M175.933 377v-47.294C103.758 323.82 46.812 262.74 46.812 188.5c0-74.242 56.946-135.322 129.121-141.206V0C77.924 5.977 0 88.232 0 188.5 0 288.77 77.924 371.025 175.933 377" />
 							</g>
 						</svg>
-						<div class="article-row-content">
-							<h1 class="article-row-content-header">
-								<a href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>">
+						<a href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>">
+
+							<div class="article-row-content">
+								<h1 class="article-row-content-header">
 									<svg width="31" height="20" xmlns="http://www.w3.org/2000/svg">
 										<path class="newspaper-icon" d="M29.4871795 0H5.98290598c-1.1142094 0-2.06426282.69671875-2.41709401 1.66666667H1.28205128C.57398504 1.66666667 0 2.22630208 0 2.91666667V17.0833333C0 18.6941667 1.33931624 20 2.99145299 20H29.4871795c.7080662 0 1.2820513-.5596354 1.2820513-1.25V1.25c0-.69036458-.5739851-1.25-1.2820513-1.25zM2.56410256 17.0833333V4.16666667h.85470086V17.0833333c0 .2297396-.19172009.4166667-.42735043.4166667s-.42735043-.1869271-.42735043-.4166667zM28.2051282 17.5H5.95202991c.01997864-.1361458.03087607-.2751562.03087607-.4166667V2.5H28.2051282v15zM9.18803419 11.25h7.26495731c.3540064 0 .6410256-.2798437.6410256-.625v-5c0-.34515625-.2870192-.625-.6410256-.625H9.18803419c-.35400641 0-.64102564.27984375-.64102564.625v5c0 .3451563.28701923.625.64102564.625zm1.49572651-4.16666667h4.2735043v2.08333334h-4.2735043V7.08333333zM8.54700855 14.375v-1.25c0-.3451562.28701923-.625.64102564-.625h7.26495731c.3540064 0 .6410256.2798438.6410256.625v1.25c0 .3451563-.2870192.625-.6410256.625H9.18803419c-.35400641 0-.64102564-.2798437-.64102564-.625zm10.25641025 0v-1.25c0-.3451562.2870192-.625.6410256-.625H25c.3540064 0 .6410256.2798438.6410256.625v1.25c0 .3451563-.2870192.625-.6410256.625h-5.5555556c-.3540064 0-.6410256-.2798437-.6410256-.625zm0-7.5v-1.25c0-.34515625.2870192-.625.6410256-.625H25c.3540064 0 .6410256.27984375.6410256.625v1.25c0 .34515625-.2870192.625-.6410256.625h-5.5555556c-.3540064 0-.6410256-.27984375-.6410256-.625zm0 3.75v-1.25c0-.34515625.2870192-.625.6410256-.625H25c.3540064 0 .6410256.27984375.6410256.625v1.25c0 .3451563-.2870192.625-.6410256.625h-5.5555556c-.3540064 0-.6410256-.2798437-.6410256-.625z" fill-rule="nonzero" />
 									</svg>
-
-									<?php
-									$theTitle = wp_strip_all_tags(get_field('title'));
+									<?php $theTitle = wp_strip_all_tags(get_post_meta(get_the_ID(), 'title', true));
 									echo $theTitle; ?>
-								</a>
-							</h1>
 
-							<p class="article-row-content-description">
-								<a href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>">
-									<?php
-									$callout = wp_strip_all_tags(get_field('intro'));
-									// echo $callout;
-									echo substr($callout, 0, 150) ?>
-								</a>
-							</p>
+								</h1>
+								<time class="article-row-content-time" datetime="2008-02-14 20:00">
+									<?php the_time('F j, Y'); ?>
+								</time>
 
-							<p class="article-row-content-author">
-								<?php
-								$tag = get_the_tags();
-								if ($tag) {
-								?>
-							<p><?php the_tags(); ?></p>
-						<?php }
-						?>
-						</p>
-						<time class="article-row-content-time" datetime="2008-02-14 20:00">
-							<?php the_time('F j, Y'); ?>
-						</time>
+								<?php edit_post_link(__('(Edit)', 'nacelle'), '<span class="edit-link">', '</span>'); ?>
 
-						<?php edit_post_link(__('(Edit)', 'nacelle'), '<span class="edit-link">', '</span>'); ?>
-
-						</div>
+							</div>
+						</a>
 					</article>
 
 				<?php endwhile; ?>
--- template-parts/content/content-split.php
+++ template-parts/content/content-split.php
@@ -12,110 +12,141 @@
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<?php if (have_rows('layouts')) : $i = ''; ?>
-		<?php while (have_rows('layouts')) : the_row(); ?>
-			<?php $i++; ?>
-			<?php // LAYOUT - image and text 
-			?>
-			<?php if (get_row_layout() == 'img_txt') : ?>
+<?php
+// how to speed up ACF queries
+// https://www.brianshim.com/webtricks/speed-up-wordpress-advanced-custom-fields-queries/
+// https://www.billerickson.net/advanced-custom-fields-frontend-dependency/
+	$rows = get_post_meta(get_the_ID(), 'layouts', true);
+	foreach ((array) $rows as $count => $row) {
+		switch ($row) {
 
-				<section class="sect sect-img_txt" style="<?php if (get_sub_field('bk_color')) : ?>background-color:<?php the_sub_field('bk_color'); ?>;<?php endif; ?><?php if (get_sub_field('border')) : ?>border-bottom:10px solid <?php the_sub_field('border'); ?>;<?php endif; ?>">
-					<div class="grid-x sect-wrap <?php if (get_sub_field('fill_img')) : ?> fill-img<?php else : ?> grid-margin-y grid-padding-y<?php endif; ?> align-middle align-spaced">
-						<?php // THE content 
-						?>
-						<div class="cell<?php if (get_sub_field('fill_img')) : ?> medium-4 py-4 py-medium-0<?php else : ?> medium-5<?php endif; ?><?php if (get_sub_field('flip')) : ?> medium-order-2<?php endif; ?>">
-							<?php the_sub_field('txt'); ?>
-							<?php // content button 
-							?>
-							<?php if (get_sub_field('btn')) : ?>
-								<a class="button" href="<?php the_sub_field('btn_url'); ?>" title="<?php the_sub_field('btn'); ?>" style="<?php if (get_sub_field('btn_color')) : ?> background-color:<?php the_sub_field('btn_color'); ?><?php endif; ?>"><?php the_sub_field('btn'); ?></a>
-							<?php endif; ?>
-						</div>
-						<div class="cell<?php if (get_sub_field('fill_img')) : ?> medium-6<?php else : ?> medium-5<?php endif; ?>">
-							<?php // THE image 
-							?>
-							<a data-open="imgModal-<?php echo $i; ?>">
-								<figure>
-									<?php
-									$image = get_sub_field('img');
-									if (!empty($image)) : ?>
-										<img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" title="<?php echo $image['caption']; ?>" />
-									<?php endif; ?>
-								</figure>
-							</a>
-							<?php // image modal 
-							?>
-							<div class="small reveal" id="imgModal-<?php echo $i; ?>" data-reveal>
-								<button class="close-button" data-close aria-label="Close reveal" type="button">
-									<span aria-hidden="true">&times;</span>
-								</button>
-								<figure class="reveal-content">
-									<?php
-									$image = get_sub_field('img');
-									if (!empty($image)) : ?>
-										<img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" title="<?php echo $image['caption']; ?>" />
-									<?php endif; ?>
-								</figure>
-							</div>
-							<?php // image bottom link 
-							?>
-							<?php if (get_sub_field('img_link')) : ?>
-								<a href="<?php the_sub_field('img_link_url'); ?>" title="<?php the_sub_field('img_link'); ?>">
-									<figcaption class="text-center"><?php the_sub_field('img_link'); ?> »</figcaption>
-								</a>
-							<?php endif; ?>
-						</div>
-					</div>
-				</section>
+				// Image with Text layout
+			case 'img_txt':
+				$bk_color = get_post_meta(get_the_ID(), 'layouts_' . $count . '_bk_color', true);
+				if($bk_color)
+					$bk_color = 'background-color:' . $bk_color . ';';
+				$border = get_post_meta(get_the_ID(), 'layouts_' . $count . '_border', true);
+				if($border)
+					$border = 'border-bottom:10px solid ' . $border . ';';
+					else $border = '';
+				$fill_img = get_post_meta(get_the_ID(), 'layouts_' . $count . '_fill_img', true);
+				if($fill_img)
+					$fill_img = ' fill-img';
+					else $fill_img = ' grid-margin-y grid-padding-y';
+				$flip = get_post_meta(get_the_ID(), 'layouts_' . $count . '_flip', true);
+				if($flip)
+					$flip = ' medium-order-2';
+					else $flip = '';
+				$txt = get_post_meta(get_the_ID(), 'layouts_' . $count . '_txt', true);
+				$btn = get_post_meta(get_the_ID(), 'layouts_' . $count . '_btn', true);
+				if ($btn) :
+					$btn_txt = get_post_meta(get_the_ID(), 'layouts_' . $count . '_btn_txt', true);
+					$btn_url = get_post_meta(get_the_ID(), 'layouts_' . $count . '_btn_url', true);
+					$btn_color = get_post_meta(get_the_ID(), 'layouts_' . $count . '_btn_color', true);
+					$btn_color = 'style="background:' . $btn_color . '";';
+					$btn = '<a class="button" href="' . $btn_url . '"' . $btn_color . '>' . $btn_txt . '</a>';
+				else : $btn = '';
+				endif;
+				$img_link = get_post_meta(get_the_ID(), 'layouts_' . $count . '_img_link', true);
+				if($img_link) :
+					$img_link_txt = get_post_meta(get_the_ID(), 'layouts_' . $count . '_img_link_txt', true);
+					$img_link_url = get_post_meta(get_the_ID(), 'layouts_' . $count . '_img_link_url', true);
+					$img_link = '<a href="' . $img_link_url . '" title="' . $img_link_txt . '"><figcaption class="text-center">' . $img_link_txt . ' »</figcaption></a>';
+					else : $img_link = '';
+				endif;
+				$img = get_post_meta(get_the_ID(), 'layouts_' . $count . '_img', true);
+				echo '<section class="sect sect-img_txt" style="' . $bk_color . $border . '">';
+					echo '<div class="grid-x sect-wrap align-middle align-spaced' . $fill_img . '">';
+						echo '<div class="cell medium-5' . $flip . '">';
+							echo apply_filters('the_content', $txt);
+							echo $btn;
+						echo '</div>';
+						echo '<div class="cell medium-6">';
+							echo '<a data-open="imgModal-' . $count . '">';
+								echo '<figure>' . wp_get_attachment_image( $img, 'large' ) . '</figure>';
+							echo '</a>';
+							echo $img_link;
+						echo '</div>';
+					echo '</div>';
+				echo '</section>';
+				// Image Modal
+				echo '<div class="small reveal" id="imgModal-' . $count . '" data-reveal>';
+					echo '<button class="close-button" data-close aria-label="Close reveal" type="button">';
+						echo '<span aria-hidden="true">&times;</span>';
+					echo '</button>';
+					echo '<figure>' . wp_get_attachment_image($img, 'large') . '</figure>';
+				echo '</div>';
+				break;
 
-				<?php // LAYOUT - text and quote 
-				?>
-			<?php elseif (get_row_layout() == 'txt_quote') : ?>
+			// Quote with Text layout
+			case 'txt_quote':
+				$bk_color = get_post_meta(get_the_ID(), 'layouts_' . $count . '_bk_color', true);
+				if($bk_color)
+					$bk_color = 'background-color:' . $bk_color;
+				$bk_img = get_post_meta(get_the_ID(), 'layouts_' . $count . '_bk_img', true);
+				if($bk_img)
+					$bk_img = 'background-image:url(' . wp_get_attachment_image_url($bk_img, '') . ')';
+				$bk_pos = get_post_meta(get_the_ID(), 'layouts_' . $count . '_bk_pos', true);
+				if ($bk_pos)
+					$bk_pos = 'background-position:' . $bk_pos;
+				$txt = get_post_meta(get_the_ID(), 'layouts_' . $count . '_txt', true);
+				$btn = get_post_meta(get_the_ID(), 'layouts_' . $count . '_btn', true);
+				if ($btn) :
+					$btn_txt = get_post_meta(get_the_ID(), 'layouts_' . $count . '_btn_txt', true);
+					$btn_url = get_post_meta(get_the_ID(), 'layouts_' . $count . '_btn_url', true);
+					$btn_color = get_post_meta(get_the_ID(), 'layouts_' . $count . '_btn_color', true);
+					$btn_color = 'style="background:' . $btn_color . '";';
+					$btn = '<a class="button" href="'. $btn_url . '"' . $btn_color . '>' . $btn_txt . '</a>';
+					else : $btn = '';
+				endif;
+				$quote = get_post_meta(get_the_ID(), 'layouts_' . $count . '_quote', true);
+				echo '<section class="sect sect-txt_quote" style="' . $bk_color . ';' . $bk_img . ';' . $bk_pos . ';">';
+					echo '<div class="grid-x sect-wrap grid-padding-y align-middle align-spaced">';
+						echo '<div class="cell medium-5">';
+							echo apply_filters('the_content', $txt);
+							echo $btn;
+						echo '</div>';
+						echo '<div class="cell medium-5">';
+							echo $quote;
+						echo '</div>';
+					echo '</div>';
+				echo '</section>';
+				break;
 
-				<section class="sect sect-txt_quote" style="<?php if (get_sub_field('bk_color')) : ?>background-color:<?php the_sub_field('bk_color'); ?><?php endif; ?>;<?php if (get_sub_field('bk_img')) : ?>background-image:url(<?php the_sub_field('bk_img'); ?><?php endif; ?>);<?php if (get_sub_field('bk_pos')) : ?>background-position:<?php the_sub_field('bk_pos'); ?><?php endif; ?>">
-					<div class="grid-x sect-wrap grid-padding-y align-middle align-spaced">
-						<?php // THE content 
-						?>
-						<div class="cell medium-5">
-							<?php the_sub_field('txt'); ?>
-							<?php // button 
-							?>
-							<?php if (get_sub_field('btn')) : ?>
-								<a class="button expanded" href="<?php the_sub_field('btn_url'); ?>" title="<?php the_sub_field('btn'); ?>"><?php the_sub_field('btn'); ?></a>
-							<?php endif; ?>
-						</div>
-						<?php // THE quote 
-						?>
-						<div class="cell medium-5">
-							<?php the_sub_field('quote'); ?>
-						</div>
-					</div>
-				</section>
-
-				<?php // LAYOUT - banner 
-				?>
-			<?php elseif (get_row_layout() == 'banner') : ?>
-
-				<section class="sect sect-banner" style="<?php if (get_sub_field('bk_color')) : ?>background-color:<?php the_sub_field('bk_color'); ?><?php endif; ?>;<?php if (get_sub_field('bk_img')) : ?>background-image:url(<?php the_sub_field('bk_img'); ?><?php endif; ?>)">
-					<div class="grid-x sect-wrap grid-margin-y grid-padding-y align-center-middle">
-						<?php // THE content 
-						?>
-						<div class="cell <?php the_sub_field('width'); ?>">
-							<div class="overlay" style="opacity:.<?php the_sub_field('bk_img_ov'); ?>"></div>
-							<?php the_sub_field('txt'); ?>
-							<?php // button 
-							?>
-							<?php if (get_sub_field('btn')) : ?>
-								<a class="button" href="<?php the_sub_field('btn_url'); ?>" title="<?php the_sub_field('btn'); ?>"><?php the_sub_field('btn'); ?></a>
-							<?php endif; ?>
-						</div>
-					</div>
-				</section>
-
-			<?php endif; ?>
-		<?php endwhile; ?>
-	<?php endif; ?>
-
-	<?php edit_post_link(__('(Edit)', 'nacelle'), '<span class="edit-link">', '</span>'); ?>
+			// Banner layout
+			case 'banner':
+				$bk_color = get_post_meta(get_the_ID(), 'layouts_' . $count . '_bk_color', true);
+				if ($bk_color)
+					$bk_color = 'background-color:' . $bk_color;
+				$bk_img = get_post_meta(get_the_ID(), 'layouts_' . $count . '_bk_img', true);
+				if ($bk_img)
+					$bk_img = 'background-image:url(' . wp_get_attachment_image_url($bk_img, '') . ')';
+				$txt = get_post_meta(get_the_ID(), 'layouts_' . $count . '_txt', true);
+				$btn = get_post_meta(get_the_ID(), 'layouts_' . $count . '_btn', true);
+				if ($btn) :
+					$btn_txt = get_post_meta(get_the_ID(), 'layouts_' . $count . '_btn_txt', true);
+					$btn_url = get_post_meta(get_the_ID(), 'layouts_' . $count . '_btn_url', true);
+					$btn_color = get_post_meta(get_the_ID(), 'layouts_' . $count . '_btn_color', true);
+					$btn_color = 'style="background:' . $btn_color . '";';
+					$btn = '<a class="button" href="' . $btn_url . '"' . $btn_color . '>' . $btn_txt . '</a>';
+				else : $btn = '';
+				endif;
+				$width = get_post_meta(get_the_ID(), 'layouts_' . $count . '_width', true);
+				$overlay = get_post_meta(get_the_ID(), 'layouts_' . $count . '_bk_img_ov', true);
+				echo '<section class="sect sect-banner" style="' . $bk_color . ';' . $bk_img . ';">';
+					echo '<div class="grid-x sect-wrap grid-margin-y grid-padding-y align-center-middle">';
+						echo '<div class="cell ' . $width . '">';
+							echo '<div class="overlay" style="opacity:.' . $overlay . '"></div>';
+							echo apply_filters('the_content', $txt);
+							echo $btn;
+						echo '</div>';
+					echo '</div>';
+				echo '</section>';
+				break;
+	
+		}
+	}
+	edit_post_link(__('(Edit)', 'nacelle'), '<span class="edit-link">', '</span>');
+?>
 
 </article>
