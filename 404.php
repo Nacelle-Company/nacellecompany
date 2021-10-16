b189db148b82050796783702c02e058d74ca06dc
luukee <luke@lukecarlhartman.com>
2021-10-16 00:33:37 -0700

    404 page update

--- 404.php
+++ 404.php
@@ -17,23 +17,67 @@ include 'template-parts/logic/logic-404.php';
 					<h1 class="entry-title"><?php _e('This is not the page you are looking for. . .', 'nacelle'); ?></h1>
 				</header>
 				<div class="entry-content">
-					<?php $bkImage = get_template_directory_uri() . '/src/assets/images/bamford-is-sad-you-are-on-404-page.png'; ?>
+					<?php $bkImage = get_template_directory_uri() . '/dist/assets/images/bamford-is-sad-you-are-on-404-page.png'; ?>
 					<style>
-						main.main-container-full {
-							background-image: url("<?php echo $bkImage; ?>");
+						body {
+							background-image: url("<?php echo $bkImage; ?>") !important;
 							background-repeat: no-repeat;
-							background-position: right bottom;
-							background-size: 45%;
+							background-position-x: -40px;
+							background-position-y: bottom;
+							background-size: 90%;
 						}
-						@media print, screen and (min-width: 40em) {
-							main.main-container-full {
-								background-position: right top;
+
+						h1.entry-title {
+							font-size: 13px;
+						}
+
+						.error .button {
+							display: flex;
+							align-items: center;
+							margin: 5px;
+						}
+
+						svg {
+							padding-right: 5px;
+						}
+
+						@media print,
+						screen and (min-width: 40em) {
+							body {
+								/* min-height: 100vh; */
+								background-size: 25%;
+								background-position: right bottom;
+							}
+
+							h1.entry-title {
+								font-size: 1.5rem;
+							}
+
+							.button {
+								padding: .5em;
+							}
+
+
+						}
+
+						@media print,
+						screen and (min-width: 64em) {
+							body {
+								min-height: 100vh;
+								background-position-x: 30vw;
+								background-position-y: bottom;
+								background-size: 54vh;
+							}
+
+							footer {
+								position: absolute;
+								bottom: 0;
 							}
 						}
 					</style>
 					<div class="error">
-						<p class="bottom"><?php _e('The page you are looking for might have been moved, had its name changed, or we\'re working on it.', 'nacelle'); ?></p>
-						<h2 class="text-center h3 mb-2"><?php _e('but maybe one of these are?', 'nacelle'); ?></h2>
+						<p class="bottom"><?php _e('The page you are looking for may have been moved, had its name changed or we’re working on it.', 'nacelle'); ?></p>
+						<h2 class="text-center h4 mb-2"><?php _e('maybe try one of these?', 'nacelle'); ?></h2>
 						<div class="grid-x align-center align-middle">
 
 							<!-- home icon and button  -->
@@ -44,7 +88,7 @@ include 'template-parts/logic/logic-404.php';
 
 							<!-- main catalog button and icon  -->
 							<a href="<?php bloginfo('url');
-												echo $catalogPath; ?>" class="button large">
+												echo $catalogPath; ?>" class="button large flex-container">
 								<?php get_template_part('template-parts/svg/icon', 'disk-lg', ['fill' => $fillColor]); ?>
 								<span><?php _e('Catalog', 'nacelle'); ?></span>
 							</a>
