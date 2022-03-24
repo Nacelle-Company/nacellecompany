9b209e601749fbef3ef8454ec1b9d553f9921421
Luke Carl Hartman <luke@lukecarlhartman.com>
2022-03-23 20:53:34 -0700

    wonder page!

--- front-page.php
+++ front-page.php
@@ -10,31 +10,24 @@
 namespace WP_Rig\WP_Rig;
 
 get_header();
-
 // Use grid layout if blog index is displayed.
 if ( is_home() ) {
 	wp_rig()->print_styles( 'wp-rig-content', 'wp-rig-front-page' );
 } else {
 	wp_rig()->print_styles( 'wp-rig-content' );
 }
-
 ?>
 	<main id="primary" class="site-main">
-
 		<?php
 		while ( have_posts() ) {
 			the_post();
-
 			get_template_part( 'template-parts/hero/hero_flickity', get_post_type() );
-
 			get_template_part( 'template-parts/content/entry', get_post_type() );
-
 			get_template_part( 'template-parts/content/entry_post-grid', get_post_type() );
-
 		}
-
 		get_template_part( 'template-parts/content/pagination' );
 		?>
+		<div>
 	</main><!-- #primary -->
 <?php
 get_footer();
