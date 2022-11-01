19afb91c1b89b75a41d40d001c5eb973de768efd
Luke Carl Hartman <luke@lukecarlhartman.com>
2022-11-01 15:10:23 -0700

    Catalog/products btns title #70

--- template-parts/catalog/parts/catalog_buttons.php
+++ template-parts/catalog/parts/catalog_buttons.php
@@ -11,68 +11,64 @@ wp_rig()->print_styles( 'wp-rig-catalog_buttons' );
 ?>
 
 <?php
-$itunes_video     = get_post_meta( get_the_ID(), 'itunes_video', true );
-$itunes_audio_url = get_post_meta( get_the_ID(), 'itunes_audio_url', true );
-if ( get_post_meta( get_the_ID(), 'show_video_links', true ) ) :
-	?>
-	<div class="entry-buttons__wrapper">
-		<h3 class="title h3">
-	<?php esc_html_e( 'Watch Now', 'wp-rig' ); ?>
-		</h3>
+$itunes_video     	= get_post_meta( get_the_ID(), 'itunes_video', true );
+$itunes_audio_url 	= get_post_meta( get_the_ID(), 'itunes_audio_url', true );
+$vid_btn_title		= get_post_meta(get_the_ID(), 'vid_btn_title', true);
+$audio_btn_title	= get_post_meta(get_the_ID(), 'audio_btn_title', true);
+?>
+<?php if ( have_rows( 'new_large_link' ) || $itunes_video ) : ?>
+<div class="entry-buttons__wrapper">
+	<h3 class="title h3">
+		<?php esc_html_e( $vid_btn_title ); ?>
+	</h3>
 	<?php if ( $itunes_video ) : ?>
-			<a href="<?php echo esc_attr( $itunes_video ); ?>" class="button apple-link" title="Watch <?php the_title_attribute(); ?> on Apple TV" target="_blank" rel="noreferrer">
-				<strong>
-					<?php esc_html_e( 'Apple TV', 'wp-rig' ); ?>
-				</strong>
+	<a href="<?php echo esc_attr( $itunes_video ); ?>" class="button apple-link" title="Watch <?php the_title_attribute(); ?> on Apple TV" target="_blank" rel="noreferrer">
+		<strong>
+		<?php esc_html_e( 'Apple TV', 'wp-rig' ); ?>
+		</strong>
+		<?php echo file_get_contents( get_template_directory() . '/assets/images/icon-external-link.svg' ); ?>
+	</a>
+	<?php endif; ?>
+	<?php
+	while ( have_rows( 'new_large_link' ) ) :
+		the_row();
+		$video_title = get_sub_field( 'link_title' );
+		$video_link  = get_sub_field( 'link_url' );
+		if(!empty($video_title && $video_link)) :
+			?>
+			<a href="<?php echo esc_html( $video_link ); ?>" class="button" title="Watch <?php the_title_attribute(); ?> on <?php echo esc_html( $video_title ); ?>" target="_blank" rel="noreferrer">
+				<strong><?php echo esc_html( $video_title ); ?></strong>
 				<?php echo file_get_contents( get_template_directory() . '/assets/images/icon-external-link.svg' ); ?>
 			</a>
-	<?php endif; ?>
-	<?php if ( have_rows( 'new_large_link' ) ) : ?>
-		<?php
-		while ( have_rows( 'new_large_link' ) ) :
-			the_row();
-			$video_title = get_sub_field( 'link_title' );
-			$video_link  = get_sub_field( 'link_url' );
-			if(!empty($video_title && $video_link)) :
-				?>
-				<a href="<?php echo esc_html( $video_link ); ?>" class="button" title="Watch <?php the_title_attribute(); ?> on <?php echo esc_html( $video_title ); ?>" target="_blank" rel="noreferrer">
-					<strong><?php echo esc_html( $video_title ); ?></strong>
-					<?php echo file_get_contents( get_template_directory() . '/assets/images/icon-external-link.svg' ); ?>
-				</a>
-			<?php endif; ?>
-		<?php endwhile; ?>
-	<?php endif; ?>
-	</div>
+		<?php endif; ?>
+	<?php endwhile; ?>
+</div>
 <?php endif; ?>
-<?php
-if ( get_post_meta( get_the_ID(), 'show_audio_links', true ) ) :
-	?>
-	<div class="entry-buttons__wrapper">
-		<h3 class="title h3">
-	<?php esc_html_e( 'Listen Now', 'wp-rig' ); ?>
-		</h3>
+<?php if ( have_rows( 'audio_new_large_link' ) || $itunes_audio_url ) : ?>
+<div class="entry-buttons__wrapper">
+	<h3 class="title h3">
+		<?php esc_html_e( $audio_btn_title ); ?>
+	</h3>
 	<?php if ( $itunes_audio_url ) : ?>
-			<a href="<?php echo esc_attr( $itunes_audio_url ); ?>" class="button apple-link" title="Listen <?php the_title_attribute(); ?> on iTunes" target="_blank" rel="noreferrer">
-				<strong>
-					<?php esc_html_e( 'iTunes', 'wp-rig' ); ?>
-				</strong>
+	<a href="<?php echo esc_attr( $itunes_audio_url ); ?>" class="button apple-link" title="Listen <?php the_title_attribute(); ?> on iTunes" target="_blank" rel="noreferrer">
+		<strong>
+			<?php esc_html_e( 'iTunes', 'wp-rig' ); ?>
+		</strong>
+		<?php echo file_get_contents( get_template_directory() . '/assets/images/icon-external-link.svg' ); ?>
+	</a>
+	<?php endif; ?>
+	<?php
+	while ( have_rows( 'audio_new_large_link' ) ) :
+		the_row();
+		$audio_title = get_sub_field( 'audio_link_title' );
+		$audio_link  = get_sub_field( 'audio_link_url' );
+		if(!empty($audio_title && $audio_link)) :
+			?>
+			<a href="<?php echo esc_html( $audio_link ); ?>" class="button" title="Watch <?php the_title_attribute(); ?> on <?php echo esc_html( $audio_title ); ?>" target="_blank" rel="noreferrer">
+				<strong><?php echo esc_html( $audio_title ); ?></strong>
 				<?php echo file_get_contents( get_template_directory() . '/assets/images/icon-external-link.svg' ); ?>
 			</a>
-	<?php endif; ?>
-	<?php if ( have_rows( 'audio_new_large_link' ) ) : ?>
-		<?php
-		while ( have_rows( 'audio_new_large_link' ) ) :
-			the_row();
-			$audio_title = get_sub_field( 'audio_link_title' );
-			$audio_link  = get_sub_field( 'audio_link_url' );
-			if(!empty($audio_title && $audio_link)) :
-				?>
-				<a href="<?php echo esc_html( $audio_link ); ?>" class="button" title="Watch <?php the_title_attribute(); ?> on <?php echo esc_html( $audio_title ); ?>" target="_blank" rel="noreferrer">
-					<strong><?php echo esc_html( $audio_title ); ?></strong>
-					<?php echo file_get_contents( get_template_directory() . '/assets/images/icon-external-link.svg' ); ?>
-				</a>
-			<?php endif; ?>
-		<?php endwhile; ?>
-	<?php endif; ?>
-	</div>
+		<?php endif; ?>
+	<?php endwhile; ?>
+</div>
 <?php endif; ?>
