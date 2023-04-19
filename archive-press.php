4b0858727b750e15c35c4255ccb80805a67db935
Luke Carl Hartman <luke@lukecarlhartman.com>
2023-04-19 09:57:39 -0700

    press archive post footer alignment

--- archive-press.php
+++ archive-press.php
@@ -11,17 +11,20 @@
 namespace WP_Rig\WP_Rig;
 
 get_header();
-
-$queried_obj    = get_queried_object()->name;
-
 wp_rig()->print_styles( 'wp-rig-archive_press' );
-$main_class = 'site-main archive-main';
+
+define('SITE', "$_SERVER[HTTP_HOST]");
+$nacelle_class = "";
+if(SITE === 'www.nacellecompany.com') {
+	$nacelle_class = ' nacelle-site';
+}
+$queried_obj    = get_queried_object()->name;
+$main_class = 'site-main archive-main' . $nacelle_class;
 get_template_part( 'template-parts/header/page-header_press' );
 ?>
 	<main id="primary" class="<?php echo esc_html( $main_class ); ?>">
 		<?php
 		if ( have_posts() ) {
-
 			while ( have_posts() ) {
 				the_post();
 				get_template_part( 'template-parts/content/entry', get_post_type() );
